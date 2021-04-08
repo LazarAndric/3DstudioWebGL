7bdc8851c39cf0ae2217b4616ca801906d1737de
Lazar Andric <Lazarandric97@gmail.com>
2021-04-08 15:10:01 +0200

    Add Picture class

---
+++ Assets/Scripts/Picture.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Picture : MonoBehaviour
+{
+    [SerializeField]
+    public string id;
+    [SerializeField]
+    public string pictureName;
+    [SerializeField]
+    public string description;
+}
